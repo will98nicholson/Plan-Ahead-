0b4de395d550183566f0804168fd037f229a842b
William Nicholson <will98nicholson@gmail.com>
2021-02-24 16:35:15 -0500

    new vars, basic bootstrap input groups

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,8 @@
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+
+var grid = $('grid')
+
+var gridTime = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
+
