e21a5f961c64aac38215e3edb12ff07365bdcd40
William Nicholson <will98nicholson@gmail.com>
2021-02-25 14:46:49 -0500

    bootstrap grid

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,35 +1,60 @@
-var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+//declare variables
+let todaysDate = $('#currentDay');
+let mainArea = $('#main-content');
+let hours = ['09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM'];
+let browserStorage = window.localStorage;
 
+//display current date
+todaysDate.text(moment().format('dddd, MMMM Do'));
 
-var grid = $('grid')
+//display time blocks
+for (var i = 0; i < hours.length; i++) {
+    mainArea.append($('<div>').addClass('time-block').attr('id', 'hour-' + i));
+    $('div.time-block').eq(i).append($('<div>').addClass('row'))
+    $('div.row').eq(i).append($('<span>').addClass('hour').text(hours[i]).css('padding-top', '5px')).append($('<textarea>').addClass('description')).append($('<button>').addClass('saveBtn').text('save'))
+};
 
-var gridTime = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
-
-
-var activityLog = function () {
-    for (var i = 0; i < gridTime.length; i++) {
-        var hour = $("<div></div>");
-        hour.addClass("hour");
-        timeBlock.append(hour);
-
-        var activityType = $("<div></div>");
-        activityType.addClass("activityType");
-        activityType.attr("data-time", gridTime.length + i);
-
-        var activityInput = $("<div id='enter' ></div>");
-
-        activityInput.attr("data-value", i);
-        activityType.append(activityInput);
-
-        var save = $("<div></div>");
-        save.addClass("saveBtn");
-        save.append("<i id='save' class='fa fa-save'></i>");
-        timeBlock.append(save);
-
-        grid.append(timeBlock);
-
-
-
+//color code time rows
+let rowCount = mainArea.children().length;
+let currentHour = moment().format('hh:mm A')
+let currentTime = Date.parse(`01-01-20 ${currentHour}`)
+for (var x = 0; x < rowCount; x++) {
+    let currentRow = $(`#hour-${x} > div > span`);
+    let rowTime = currentRow.text();
+    let checkTime = Date.parse(`01-01-20 ${rowTime}`);
+    if (checkTime < currentTime) {
+        currentRow.next($('textarea')).addClass("past");
+    } else if (checkTime > currentTime) {
+        currentRow.next($('textarea')).addClass("future");
+    } else {
+        currentRow.next($('textarea')).addClass("present");
     }
+};
+
+
+//display items already in local storage
+let savedInfoArray = [];
+if (browserStorage.length > 0) {
+    savedInfoArray = JSON.parse(browserStorage.getItem('savedItems'))
 }
+
+//event listener to save input
+$('.saveBtn').on('click', function (event) {
+    event.preventDefault();
+    let textInput = $(this).siblings('textarea').val();
+    let rowInputId = $(this).parent().parent().attr('id');
+    savedInfoArray.push({ rowInputId, textInput });
+    browserStorage.setItem('savedItems', JSON.stringify(savedInfoArray))
+});
+
+
+//write local storage items to correct row
+let displayInfoArray = [];
+if (browserStorage.length > 0) {
+    displayInfoArray = JSON.parse(browserStorage.getItem('savedItems'));
+    for (var i = 0; i < displayInfoArray.length; i++) {
+        let targetRow = displayInfoArray[i].rowInputId;
+        $(`#${targetRow}`).children().children('textarea').val(displayInfoArray[i].textInput)
+    }
+};
+© 2021 GitHub, Inc.
